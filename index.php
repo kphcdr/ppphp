03fc0b7b57f79379e8532426a3117744a9c877ed
kphcdr <kphcdr@163.com>
2013-12-01 01:50:19 +0800

    修改了readme

--- index.php
+++ index.php
@@ -1,18 +1,17 @@
 <?php
 /*
-*	入口文件 
+*	PPPHP入口文件 
 *
-*
+*	@author kphcdr <kphcdr.163.com>
 */
-	define('ENVIRONMENT', true);//调试模式
+	header("Content-type:text/html;charset=utf-8");
+	define('ENVIRONMENT', 1);//调试模式
 	define('PPPHP',realpath('./'));	//F:\www\git\ppphp  根目录
-
 	//常用配置
 	$Web = 'http://www.ppphp.com/'; //网站域名
-	$Core = PPPHP.'/App/Core';//PPPHP框架目录
-	$App = PPPHP.'/App';//程序目录
-	$View = PPPHP.'/App/view/';//模板目录
-
+	$Core = PPPHP.'/app/Core';//PPPHP框架目录
+	$App = PPPHP.'/app';//程序目录
+	$View = PPPHP.'/app/view/';//模板目录
 	//系统路径
 	define('CORE',$Core);
 	define('APP', $App);
@@ -21,7 +20,7 @@
 	//是否开启错误提示
 	if (ENVIRONMENT)
 	{
-
+			error_reporting(E_ALL);
 	}
 	else
 	{
