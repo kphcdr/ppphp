db79af732e0c80c2d40c8d6d67d7cae1793c6399
kphcdr <kphcdr@163.com>
2014-09-05 15:35:20 +0800

    默认不开启模板缓存

--- Core/lib/T.class.php
+++ Core/lib/T.class.php
@@ -9,8 +9,8 @@ class t extends Smarty
 			$this->template_dir = VIEW; // 模版目录
 			$this->compile_dir = APP.'/tmp'; //编译目录
 			$this->cache_dir = APP.'/tmp'; //缓存目录
-			$this->cache_lifetime = 0; //缓存时间 
-			$this->caching = false; //缓存方式 
+			$this->cache_lifetime = 1800; //缓存时间 
+			$this->caching = FALSE; //缓存方式 
 			$this->allow_php_templates = FALSE;//模板中使用[php]  
 			$this->left_delimiter = '{'; //左定界符
 			$this->right_delimiter = '}'; //右定界符
