22e9a5742974efcae98d9accb9ce0c9e7391f7c6
kphcdr <kphcdr@163.com>
2015-06-04 21:38:53 +0800

    入口文件清爽了一些

--- index.php
+++ index.php
@@ -4,22 +4,12 @@
 *
 *	@author kphcdr <kphcdr.163.com>
 */
-	header("Content-type:text/html;charset=utf-8");
-	define('ENVIRONMENT', true);//调试模式
-	define('PPPHP',realpath('./'));	// 根目录
-	//系统路径
-	define('CORE',PPPHP.'/Core');
-	define('APP', PPPHP.'/app');
-	define('VIEW', PPPHP.'/app/view/');
-	define('WEB', $_SERVER['PHP_SELF']);
-	//是否开启错误提示以及BUG调试
-	if (ENVIRONMENT)
-	{
-		error_reporting(E_ALL);
-	}
-	else
-	{
-		error_reporting(0);
-	}
-	//let go
-	require CORE.'/ppphp.php';
+define('DEBUG', true);//调试模式
+define('PPPHP',realpath('./'));	// 根目录
+//系统路径
+define('CORE',PPPHP.'/Core');
+define('APP', PPPHP.'/app');
+define('VIEW', PPPHP.'/app/view');
+define('WEB', $_SERVER['PHP_SELF']);
+//let go
+require CORE.'/core/ppphp.php';
