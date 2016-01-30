99448ddbd102e8575b97cc8757c2c2be167d77aa
kphcdr <kphcdr@163.com>
2016-01-30 23:39:48 +0800

    增加了get和post函数

--- core/function/function.php
+++ core/function/function.php
@@ -12,3 +12,47 @@ function p($var)
         echo "<pre style='position:relative;z-index:1000;padding:10px;border-radius:5px;background:#F5F5F5;border:1px solid #aaa;font-size:14px;line-height:18px;opacity:0.9;'>" . print_r($var, true) . "</pre>";
     }
 }
+
+function get($str,$filter = '',$default = false)
+{
+    if(isset($_GET[$str]))
+    {
+        $return = $_GET[$str];
+        switch ($filter)
+        {
+            case 'int':
+                if (!is_numeric($return))
+                {
+                    return $default;
+                }
+                break;
+            default:$return = htmlspecialchars($return);
+
+        }
+        return $return;
+    } else {
+        return $default;
+    }
+}
+
+function post($str,$filter = '',$default = false)
+{
+    if(isset($_POST[$str]))
+    {
+        $return = $_POST[$str];
+        switch ($filter)
+        {
+            case 'int':
+                if (!is_numeric($return))
+                {
+                    return $default;
+                }
+                break;
+            default:$return = htmlspecialchars($return);
+
+        }
+        return $return;
+    } else {
+        return $default;
+    }
+}
--- core/ppphp/route.php
+++ core/ppphp/route.php
@@ -10,13 +10,13 @@ class route
         if(isset($_SERVER['REQUEST_URI'])) {
             $pathstr = str_replace($_SERVER['SCRIPT_NAME'],'',$_SERVER['REQUEST_URI']);
             $path = explode('/',trim($pathstr,'/'));
-            if(isset($path[0])) {
+            if(isset($path[0]) && $path[0]) {
                 $this->ctrl = $path[0];
             } else {
                 $this->ctrl = conf::conf('DEFAULT_CTRL','route');
             }
             
-            if(isset($path[1])) {
+            if(isset($path[1]) && $path[1]) {
                 $this->action = $path[1];
             } else {
                 $this->action = conf::conf('DEFAULT_ACTION','route');
