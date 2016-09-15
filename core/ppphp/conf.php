76716a689739b10b3845392b9150b8258c444402
moling <365024424@qq.com>
2016-09-15 11:15:06 +0800

    整合配置类的get和all方法
    原来的get方法有bug，当缓存过的文件但配置项的名不对时，没有正确的返回false，而get和all的代码重复率太高，完全可以整合重用。

--- core/ppphp/conf.php
+++ core/ppphp/conf.php
@@ -14,46 +14,29 @@ class conf
     static public $conf = array();
     
     /**
-     * 加载系统配置,如果之前已经加载过,那么就直接返回
-     * @param string $name 配置名
+     * 加载系统配置文件,如果之前已经加载过,那么就直接返回所有配置或其中一个配置
      * @param string $file 文件名
-     * @return mix
+     * @param string $name 配置名
+     * @return mixed       找不到配置时返回false，当$name为空时返回文件所有配置，否则只返回$name这配置项
      */
-    static public function get($name,$file='conf')
+    static public function get($file='conf', $name=null)
     {
-        if(isset(self::$conf[$file][$name])) {
-            return self::$conf[$file][$name];
-        } else {
-            $conf = CORE.'config/'.$file.'.php';
-            if(is_file($conf)) {
-                self::$conf[$file] = include $conf;
-                    return isset(self::$conf[$file][$name])?self::$conf[$file][$name]:false;
-            } else {
+        // 判断配置没有缓存的话就导入配置文件并缓存
+        if (!isset(self::$conf[$file]))
+            // 判断配置文件是否存在
+            $conf = CORE.'config/' . $file . '.php';
+            if (!is_file($conf)) {
                 return false;
             }
+
+            self::$conf[$file] = include $conf;
         }
         
-    }
-    
-    /**
-     * 加载系统配置文件(直接加载整个配置文件),如果之前已经加载过,那么就直接返回
-     * @param string $name 配置名
-     * @param string $file 文件名
-     * @return mix
-     */
-    static public function all($file)
-    {
-        if(isset(self::$conf[$file])) {
+        // 如果$name为null则返回所有的设置
+        if (is_null($name)) {
             return self::$conf[$file];
-        } else {
-            $conf = CORE.'config/'.$file.'.php';
-            if(is_file($conf)) {
-                self::$conf[$file] = include $conf;
-                return self::$conf[$file];
-            } else {
-                return false;
-            }
         }
         
+        // 判断配置项是否存在并返回
+        return isset(self::$conf[$file][$name]) ? self::$conf[$file][$name] : false;
     }
-}
