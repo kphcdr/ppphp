5c369d85b6a521272facbd4a88355e5e717d24a0
kphcdr <kphcdr@163.com>
2016-11-22 16:57:36 +0800

    调整细节

--- config/log.php
+++ config/log.php
@@ -2,5 +2,5 @@
 //日志相关配置
 return array(
     'LOG_TYPE'=>'monolog',#日志驱动
-    'LOG_PATH'=>'./log/'#存储位置
+    'LOG_PATH'=>PPPHP.'/log/log/'#存储位置
 );
--- core/ppphp/cookie.php
+++ core/ppphp/cookie.php
@@ -1,114 +1,85 @@
 <?php
-/**
- * cookie 操作类
- */
 namespace ppphp;
-/**
- * cookie类
- * 
- * @Author zhangsen 2013-08-27
- */
-class cookie{
-	private	$time;
-	/**
-	 * cookie类自加载，默认30分钟失效
-	 * 
-	 * @Author 张森 2013-05-13
-	 */
-	public function __construct()
-	{
-		$this->time	= time()+1800;
-	}
 
-	/**
-	 * 设置一个cookie
-	 * 
-	 * @param 	string 		$cookieName 	cookie名称
-	 * @param 	string 		$cookieValue 	cookie值
-	 * @param 	number 		$time 			cookie存活时间
-	 * @param 	boolen 		$httponly 		是否只能通过http协议读取，如果true，客户端js无法获取cookie值
-	 * @param 	string 		$path 			cookie路径
-	 * @param 	unknown		$domain 		默认空，不用管
-	 * @param 	boolen 		$secure 		是否只能通过https协议访问
-	 * 
-	 * @return 	boolean 	true|false
-	 * 
-	 * @Author 张森 2013-05-13
-	 */
-	function set($cookieName, $cookieValue = '' , $time = '', $httponly = FALSE, $path = '', $domain = '', $secure = FALSE)
-	{
-		if($time == '' || !is_numeric($time))
-		{
-			$time = $this->time;
-		}
-		else 
-		{
-			$time = time()+$time;
-		}
-		if($cookieName != '')
-		{
-			setcookie($cookieName,$cookieValue,$time,$httponly,$path,$domain,$secure);
-			return TRUE;
-		}else
-		{
-			return FALSE;
-		}
-	}
-	
-	/**
-	 * 获得cookie，如果没有则返回空
-	 * 
-	 * @param string $cookieName 需要获取cookie的名字
-	 * 
-	 * @return cookie value
-	 * 
-	 * @Author 张森  2013-05-13
-	 */
-	function get($cookieName)
-	{
-		if($cookieName != '')
-		{
-			return empty($_COOKIE[$cookieName])? '' : $_COOKIE[$cookieName];
-		}
-		else
-		{
-			return '';
-		}
-	}
-	/**
-	 * 注释掉一个cookie
-	 * 
-	 * @param string $cookieName 需要注释掉cookie的名字
-	 * 
-	 * @return boolean true|false
-	 * 
-	 * @Author 张森   2013-05-13
-	 */
-	function delete($cookieName)
-	{
-		if($cookieName != '')
-		{
-			$this->set($cookieName, '',0);
-			return TRUE;
-		}
-		else
-		{
-			return FALSE;
-		}
-	}
-	/**
-	 * 清除所有cookies
-	 * 
-	 * @param arr $cookieName 需要保留下cookie的名字 array('id','name')
-	 * 
-	 * @Author kphcdr   2014-2-14
-	 */	
-	public function clear()
-	{
-		if(isset($_COOKIE))
-		{
-		unset($_COOKIE);
-		}
-		
-	}
+class cookie
+{
+    const SECRETKEY = 'jonson';//混淆字符串
+
+    /**
+     * 获取cookie
+     * name cookie名称
+     */
+    public static function get($name)
+    {
+        if ($name != '') {
+            return empty($_COOKIE[$name]) ? '' : self::decrypt($_COOKIE[$name]);
+        } else {
+            return '';
+        }
+    }
+
+    /**
+     * 字符串解密
+     */
+    private static function decrypt($string)
+    {
+        $code = '';
+        $key = substr(md5(self::SECRETKEY), 8, 18);
+        $strLen = strlen($string);
+        $keyLen = strlen($key);
+        for ($i = 0; $i < $strLen; $i++) {
+            $k = $i % $keyLen;
+            $code .= $string[$i] ^ $key[$k];
+        }
+        return base64_decode($code);
+    }
+
+    /**
+     * 删除cookie
+     */
+    public static function drop($name)
+    {
+        if ($name == '' || empty($_COOKIE[$name])) {
+            return true;
+        } else {
+            return self::set($name, '', '-10');
+        }
+    }
+
+    /**
+     * 设置cookie
+     * name    必需。规定 cookie 的名称。
+     * value    必需。规定 cookie 的值。
+     * expire    可选。规定 cookie 的有效期。
+     * path    可选。规定 cookie 的服务器路径。
+     * domain    可选。规定 cookie 的域名。
+     * secure    可选。规定是否通过安全的 HTTPS 连接来传输 cookie。
+     */
+    public static function set($name, $value = null, $expire = 3600, $path = '/', $domain = '', $secure = 0)
+    {
+        if ($name != '' && $value != '') {
+            $value = self::encryption($value);
+            $expire = time() + $expire;
+            return setcookie($name, $value, $expire, $path, $domain, $secure);
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 字符串加密
+     */
+    private static function encryption($string)
+    {
+        $string = base64_encode($string);
+        $code = '';
+        $key = substr(md5(self::SECRETKEY), 8, 18);
+        $strLen = strlen($string);
+        $keyLen = strlen($key);
+        for ($i = 0; $i < $strLen; $i++) {
+            $k = $i % $keyLen;
+            $code .= $string[$i] ^ $key[$k];
+        }
+        return $code;
+    }
 }
