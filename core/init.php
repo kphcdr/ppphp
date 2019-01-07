aaf428e806e31ec6e5fb90da16a85fc47c565f32
kphcdr <kphcdr@163.com>
2019-01-07 12:13:32 +0800

    fix

--- core/init.php
+++ core/init.php
@@ -5,8 +5,8 @@ session_start();
  * 框架加载文件，用于引导框架启动
  * ======================================================================== */
 define('TIME', $_SERVER['REQUEST_TIME']);
-define('PPPHP_VERSION','2.1.2');
-define('PPPHP', realpath(__DIR__.'/../'));    // 根目录
+define('PPPHP_VERSION', '2.1.2');
+define('PPPHP', realpath(__DIR__ . '/../'));    // 根目录
 
 //环境配置加载
 \ppphp\ppphp::development();
@@ -23,29 +23,35 @@ define('MODULE', $MODULE_NAME);
 \ppphp\ppphp::init();
 
 
-if(DEBUG && PHP_SAPI != 'cli') {
+if (DEBUG) {
     //打开PHP的错误显示
-    ini_set('display_errors',true);
+    ini_set('display_errors', true);
     //载入友好的错误显示类
-    $whoops = new \Whoops\Run;
-    $errorPage = new \Whoops\Handler\PrettyPageHandler;
-    $errorPage->setPageTitle("PPPHP出大事啦!!!");
-    $whoops->pushHandler($errorPage);
+    $whoops    = new \Whoops\Run;
+    if(PHP_SAPI == 'cli') {
+        $handler = new \Whoops\Handler\PlainTextHandler();
+    } else {
+        $handler = new \Whoops\Handler\PrettyPageHandler();
+        $handler->setPageTitle("PPPHP出大事啦!!!");
+    }
+    $whoops->pushHandler($handler);
     $whoops->register();
-} else {
-    ini_set('display_errors',false);
 }
-//加载类库
-include CORE .'function/function.php';
+else {
+    ini_set('display_errors', false);
+}
+//加载函数库
+include CORE . 'function/function.php';
 
 //设置默认市区
-date_default_timezone_set(\ppphp\conf::get('TIMEZONE','system'));
+date_default_timezone_set(\ppphp\conf::get('TIMEZONE', 'system'));
 
 
-if(PHP_SAPI == 'cli') {
+if (PHP_SAPI == 'cli') {
 
     \ppphp\ppphpCli::run();
-} else {
+}
+else {
     //开始跑框架
 
     \ppphp\ppphp::run();
